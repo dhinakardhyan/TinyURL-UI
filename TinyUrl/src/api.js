a3e5345cb363d156eab5d12168a284fac02b38f5
dhinakarq <dhinakar.seetharaman@gmail.com>
2026-05-03 17:47:10 +0530

    Fix API base URL to include Url route

--- TinyUrl/src/api.js
+++ TinyUrl/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+
 const api = axios.create({
-  baseURL: "https://tinyurlapi-dhinakar-czfpfubacsh9hafw.southindia-01.azurewebsites.net/api",
+  baseURL: "https://tinyurlapi-dhinakar-czfpfubacsh9hafw.southindia-01.azurewebsites.net/api/Url",
 });
 
 export default api;
