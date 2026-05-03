daec21ba6939e1ec595b43a5e8f9391de426af5b
dhinakarq <dhinakar.seetharaman@gmail.com>
2026-05-03 16:39:40 +0530

    Fix api.js export

--- TinyUrl/src/api.js
+++ TinyUrl/src/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-export default axios.create({
-  // baseURL: "https://localhost:7181/api/url"
+const api = axios.create({
   baseURL: "https://tinyurlapi-dhinakar-czfpfubacsh9hafw.southindia-01.azurewebsites.net/api",
 });
-
 
 export default api;
