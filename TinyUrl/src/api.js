65cfc7a08a1ba7bd9ca2bf7c57e43311b0a2eb72
dhinakarq <dhinakar.seetharaman@gmail.com>
2026-05-03 16:18:29 +0530

    Updated API base URL to production

--- TinyUrl/src/api.js
+++ TinyUrl/src/api.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://localhost:7181/api/url"
+  // baseURL: "https://localhost:7181/api/url"
+  baseURL: "https://tinyurlapi-dhinakar-czfpfubacsh9hafw.southindia-01.azurewebsites.net/api",
 });
+
+
+export default api;
