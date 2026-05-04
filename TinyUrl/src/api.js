25637752e0cc4314b5a29f4bb8b41fab527fb330
dhinakarq <dhinakar.seetharaman@gmail.com>
2026-05-04 21:01:28 +0530

    Fix url path force update

--- TinyUrl/src/api.js
+++ TinyUrl/src/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-
 const api = axios.create({
-  baseURL: "https://tinyurlapi-dhinakar-czfpfubacsh9hafw.southindia-01.azurewebsites.net/api/Url",
+  baseURL: "https://tinyurlapi-dhinakar-czfpfubacsh9hafw.southindia-01.azurewebsites.net/api",
 });
 
 export default api;
